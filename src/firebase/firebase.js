2350e8436afb4086a40a05c3a58c281d1acbd86a
Michael <michael.c@modiface.com>
2024-10-03 11:16:59 -0400

    added project identity, this is not auth keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
+    apiKey: "AIzaSyDowBTq6koUsiq4n8rRK_mUWAr1ty2eO7c",
+    authDomain: "fir-auth-demo-ec2c0.firebaseapp.com",
+    projectId: "fir-auth-demo-ec2c0",
+    storageBucket: "fir-auth-demo-ec2c0.appspot.com",
+    messagingSenderId: "807220666020",
+    appId: "1:807220666020:web:9a8db8359bff0c968ad031"
   };
 
 const app = initializeApp(firebaseConfig);
